6fc10f6cfe8e8b7b396aa7de7ba8db4cf0b1fd4b
Nagy Kristóf <kristof.gopher@gmail.com>
2023-09-03 16:22:57 +0200

    temp

--- C1/C_1/C_1/main.c
+++ C1/C_1/C_1/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    printf("Hello World\n");
 
-	printf("Hello World!\n");
+    int szam1 = 10;
+    int szam2 = 5;
 
-	return 0;
+    int osszeg = szam1 + szam2;
+    int kulonbseg = szam1 - szam2;
+    int szorzat = szam1 * szam2;
+    int hanyados = szam1 / szam2;
+
+    printf("Az osszeg: %d\n", osszeg);
+    printf("A kulonbseg: %d\n", kulonbseg);
+    printf("A szorzat: %d\n", szorzat);
+    printf("A hanyados: %d\n", hanyados);
+
+    return 0;
 }
